ef7df7bcec20098bd0008025b453c05b8399eeda
oisdk <oisinkidney@Oisins-MacBook-Air.local>
2015-08-27 21:28:10 +0100

    teavis

--- SwiftDataStructures/ListMethods.swift
+++ SwiftDataStructures/ListMethods.swift
@@ -19,3 +19,23 @@ func product<T>(seqs: List<List<T>>) -> List<List<T>> {
   let rest = product(ss())
   return s.flatMap { x in rest.map { x |> $0 } }
 }
+
+extension List {
+  private func mMap<T>(f: Element -> T?) -> List<T>? {
+    guard case let .Cons(x, xs) = self else { return .Nil }
+    guard let fx = f(x), fxs = xs().mMap(f) else { return nil }
+    return fx |> fxs
+  }
+}
+
+private func uncons<T>(l: List<T>) -> (T, List<T>)? {
+  guard case let .Cons(x, xs) = l else { return nil }
+  return (x, xs())
+}
+
+private func frst<T, U>(t: (T, U)) -> T { return t.0 }
+private func scnd<T, U>(t: (T, U)) -> U { return t.1 }
+
+func transpose<T>(seqs: List<List<T>>) -> List<List<T>> {
+  return seqs.mMap(uncons).map { ps in ps.map(frst) |> transpose(ps.map(scnd)) } ?? .Nil
+}
